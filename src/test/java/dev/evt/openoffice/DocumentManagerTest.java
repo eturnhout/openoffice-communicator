a38ecd6a674d1526484882c91426e10fcb48140b
eturnhout <eelketurnhout3@gmail.com>
2021-05-04 13:58:30 +0000

    Add convertAndDelete function in DocumentManagerTest for DRY purposes

--- src/test/java/dev/evt/openoffice/DocumentManagerTest.java
+++ src/test/java/dev/evt/openoffice/DocumentManagerTest.java
@@ -30,6 +30,23 @@ public class DocumentManagerTest
     @Test
     public void testTextDocumentToHtml() throws Exception
     {
+        this.convertAndDelete(TextDocument.EXTENSION_HTML, TextDocument.PROPERTY_VALUE_FORMAT_HTML);
+    }
+
+    /**
+     * Test if a new TextDocument can be opened, saved, closed, and deleted
+     *
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
+    @Test
+    public void testTextDocumentPdfExport() throws Exception
+    {
+        this.convertAndDelete(TextDocument.EXTENSION_PDF, TextDocument.PROPERTY_VALUE_FORMAT_PDF);
+    }
+
+    private void convertAndDelete(String extension, String format) throws Exception
+    {
         String folder = this.configReader.getConfig("folder");
         String name = "testing";
         DocumentManager manager = new DocumentManager(this.connection, folder);
@@ -52,62 +69,21 @@ public class DocumentManagerTest
         manager.close(defaultDocument);
 
         // Save default document as html
-        TextDocument htmlDocument = (TextDocument) manager.open(name + TextDocument.EXTENSION_DEFAULT);
+        TextDocument newDocument = (TextDocument) manager.open(name + TextDocument.EXTENSION_DEFAULT);
 
         // We can re-use the properties from the default document and add the html format prop
-        properties.addProperty(new DocumentProperty(TextDocument.PROPERTY_FORMAT, TextDocument.PROPERTY_VALUE_FORMAT_HTML));
-        htmlDocument.setProperties(properties);
-        htmlDocument.setExtension(TextDocument.EXTENSION_HTML);
+        properties.addProperty(new DocumentProperty(TextDocument.PROPERTY_FORMAT, format));
+        newDocument.setProperties(properties);
+        newDocument.setExtension(extension);
 
-        manager.save(htmlDocument);
+        manager.save(newDocument);
 
-        assertThat(htmlDocument, instanceOf(TextDocument.class));
-        assertEquals(name, htmlDocument.getName());
-        assertEquals(TextDocument.EXTENSION_HTML, htmlDocument.getExtension());
+        assertThat(newDocument, instanceOf(TextDocument.class));
+        assertEquals(name, newDocument.getName());
+        assertEquals(extension, newDocument.getExtension());
 
-        manager.close(htmlDocument);
-        manager.delete(htmlDocument);
-        manager.delete(defaultDocument);
-    }
-
-    /**
-     * Test if a new TextDocument can be opened, saved, closed, and deleted
-     *
-     * @throws IllegalArgumentException
-     * @throws IOException
-     */
-    @Test
-    public void testTextDocumentPdfExport() throws Exception
-    {
-        String folder = this.configReader.getConfig("folder");
-        String name = "testing";
-        DocumentManager manager = new DocumentManager(this.connection, folder);
-
-        TextDocument defaultDocument = (TextDocument) manager.openNew(DocumentManager.NEW_TEXT_DOCUMENT);
-        defaultDocument.setName(name);
-
-        // We need to set the property "Overwrite" so storeToURL won't fail
-        DocumentProperties properties = new DocumentProperties();
-        properties.addProperty(new DocumentProperty(TextDocument.PROPERTY_OVERWRITE, BaseDocument.VALUE_BOOLEAN_TRUE));
-        defaultDocument.setProperties(properties);
-
-        manager.save(defaultDocument);
-
-        assertThat(defaultDocument, instanceOf(TextDocument.class));
-        assertEquals("testing", defaultDocument.getName());
-        assertEquals(TextDocument.EXTENSION_DEFAULT, defaultDocument.getExtension());
-
-        manager.close(defaultDocument);
-
-        TextDocument pdfDocument = (TextDocument) manager.open(name + TextDocument.EXTENSION_DEFAULT);
-        pdfDocument.setExtension(TextDocument.EXTENSION_PDF);
-        properties.addProperty(new DocumentProperty(TextDocument.PROPERTY_FORMAT, TextDocument.PROPERTY_VALUE_FORMAT_PDF));
-        pdfDocument.setProperties(properties);
-
-        manager.save(pdfDocument);
-
-        manager.close(pdfDocument);
-        manager.delete(pdfDocument);
+        manager.close(newDocument);
+        manager.delete(newDocument);
         manager.delete(defaultDocument);
     }
 }
