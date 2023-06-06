1d8ebb8f2f8459aed512a7f9a15ff1adf65dcc61
Eelke van Turnhout <8569367+eturnhout@users.noreply.github.com>
2023-06-06 21:17:54 +0200

    Cleanup

--- src/main/java/dev/evt/openoffice/BaseConnection.java
+++ src/main/java/dev/evt/openoffice/BaseConnection.java
@@ -10,71 +10,25 @@ import com.sun.star.uno.UnoRuntime;
  * Sets and makes the connection available for use in other classes when
  * extending
  * </p>
- * 
- * @author Eelke van Turnhout <eelketurnhout3@gmail.com>
- * @version 1.0
+ *
+ * @author Eelke van Turnhout
  */
-public class BaseConnection
-{
+public class BaseConnection {
     protected Connection connection;
     protected XComponentLoader componentLoader;
 
     /**
      * Constructs a BaseConnection object.
-     * 
+     *
      * @param connection
-     *            The connection to a running OpenOffice service.
+     *                   The connection to a running OpenOffice service.
+     *
+     * @throws Exception
      */
-    public BaseConnection(Connection connection)
-    {
-        this.setConnection(connection);
-        this.initializeComponentLoader();
-    }
-
-    /**
-     * Set the connection to interact with OpenOffice.
-     * 
-     * @param Connection
-     *            The connection to a running OpenOffice service.
-     */
-    public void setConnection(Connection connection)
-    {
+    public BaseConnection(Connection connection) throws Exception {
         this.connection = connection;
-    }
 
-    /**
-     * Get the connection from this BaseConnection object.
-     * 
-     * @return the connection to a running OpenOffice service.
-     */
-    public Connection getConnection()
-    {
-        return this.connection;
-    }
-
-    /**
-     * Get the XComponentLoader. This can be used to create/open documents.
-     * 
-     * @return the XComponentLoader.
-     */
-    public XComponentLoader getComponentLoader()
-    {
-        return this.componentLoader;
-    }
-
-    /**
-     * Initializes the XComponentLoader object.
-     */
-    private void initializeComponentLoader()
-    {
-        Object desktop = null;
-
-        try {
-            desktop = this.connection.getServiceFactory().createInstance("com.sun.star.frame.Desktop");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        this.componentLoader = (XComponentLoader) UnoRuntime.queryInterface(XComponentLoader.class, desktop);
+        this.componentLoader = (XComponentLoader) UnoRuntime.queryInterface(XComponentLoader.class,
+                connection.getServiceFactory().createInstance("com.sun.star.frame.Desktop"));
     }
 }
